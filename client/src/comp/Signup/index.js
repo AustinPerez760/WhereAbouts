a454b3ed09b975462fea98c3f8bdda24940a0f80
highwolfx <seanchangx@gmail.com>
2021-03-09 20:26:42 -0800

    Added Search Bar and modified margins on login cards

--- client/src/comp/Signup/index.js
+++ client/src/comp/Signup/index.js
@@ -5,37 +5,42 @@ import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import "./Signup.css";
 
-function Signup({signInScreen}) {
+function Signup({ signInScreen }) {
   return (
     <Container>
-    <Card>
-      <Card.Body>
-        <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Button variant="primary" type="submit" size="xl">
-            Submit
-          </Button>
-          <Button variant="secondary" type="signup" size="xl" onClick={(e) => {
-              e.preventDefault();
-              signInScreen(false);
-              }}>
-            Sign In
-          </Button>
-        </Form>
-      </Card.Body>
-    </Card>
-  </Container>
-  )
-};
+      <Card className="login-card">
+        <Card.Body>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit" size="xl">
+              Submit
+            </Button>
+            <Button
+              variant="secondary"
+              type="signup"
+              size="xl"
+              onClick={(e) => {
+                e.preventDefault();
+                signInScreen(false);
+              }}
+            >
+              Sign In
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+}
 
 export default Signup;
