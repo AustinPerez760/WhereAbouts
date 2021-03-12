0fdf99752239dee31f0343886f22f03dbfedbba1
highwolfx <seanchangx@gmail.com>
2021-03-11 22:34:31 -0800

    Added user sign-up functionality to add to database

--- client/src/comp/SignCard/index.js
+++ client/src/comp/SignCard/index.js
@@ -3,18 +3,14 @@ import { useState } from "react";
 import Signup from "../Signup/";
 import LogIn from "../LogIn";
 
-function SignCard(){
-    const [signUpLoad, setState] = useState (false);
+function SignCard() {
+  const [signUpLoad, setState] = useState(false);
 
-    if (signUpLoad === false){
-        return (
-            <LogIn signInScreen={setState}/>
-        );
-    } else {
-        return (
-            <Signup signInScreen={setState}/>
-        );
-    };
-};
+  if (signUpLoad === false) {
+    return <LogIn signInScreen={setState} />;
+  } else {
+    return <Signup signInScreen={setState} />;
+  }
+}
 
 export default SignCard;
